1192d99da72c7c722cfdba48091e96bb0985844a
Matt Enyeart <menyeart1@gmail.com>
2022-11-14 15:38:02 -0700

    Add code for a basic substring counter for single words

--- strings.rb
+++ strings.rb
@@ -11,4 +11,27 @@
 # 3. If its not the first match, incerement another count in the value for the key value pair ( :word: 2)
 #4. 
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own", "elow", "below", "low","part","partner","sit"]
+
+word = 'below'
+
 def substrings(word, dictionary)
+  
+  included_array = [] 
+  
+  dictionary.map do |substring|
+    if word.include?(substring)
+      included_array.push(substring)
+    end
+  end
+
+  substring_array = included_array.reduce(Hash.new(0)) do |result, item|
+    result[item] += 1
+    result
+  end
+
+  p substring_array
+  
+end
+
+substrings(word, dictionary)
